05afb858dfe54eb6ee571df005eef1860aae419a
axelnq <axelnquintana@gmail.com>
2022-02-10 21:10:44 -0300

    Prepare to depl

---
+++ src/app.ts
@@ -0,0 +1,70 @@
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import { PrismaClient } from "@prisma/client";
+import { routes } from "./routes/index";
+import cors from "cors";
+import passport from "passport";
+import passportLocal from "passport-local";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt"
+import session from "express-session";
+import bodyParser from "body-parser";
+const PORT = process.env.PORT || 3001;
+
+export const db = new PrismaClient();
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const localStrategy = require("passport-local").Strategy;
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser("secretcode"))
+app.use(passport.initialize());
+app.use(passport.session());
+require("./configPassport")(passport);
+
+app.use("/api", routes);
+
+app.get('/', (req, res) => {
+  res.send('Well done!');
+})
+// Error catching endware.
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send({ message });
+});
+/*
+app.listen(3001, () => {
+  console.log("Server listening on port 3001");
+});
+*/
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
